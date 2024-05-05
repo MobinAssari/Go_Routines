a8fcfe96df71582fa6f7287f880b5065f573c8d2
Mobin <mobin.aranii@gmail.com>
2024-05-05 08:51:49 +0330

    add first channel

--- main.go
+++ main.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
-
+	channel := make(chan bool)
+	greet("Nice to meet you!")
+	greet("How are you?")
+	go slowGreet("How ... are ... you ...?", channel)
+	greet("I hope you're liking the course!")
+	<-channel
 }
+
 func greet(phrase string) {
 	fmt.Println("Hello!", phrase)
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, ch chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	ch <- true
 }
